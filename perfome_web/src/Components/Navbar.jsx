ff6b6ea4c0f89c0115b7367b3fabb2f7f38ff1cf
Ramadan <daudalaramadan2@gmail.com>
2024-05-04 23:40:23 +0000

    Navbar in the making innit

---
+++ perfome_web/src/Components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { IoPersonOutline } from "react-icons/io5";
+import { TiShoppingCart } from "react-icons/ti";
+import pic from '../Assets/creed logo.png';
+import './Navbar.css'
+
+
+const Navbar = () => {
+  return (
+    <div className="nav-wrapper">
+        <div className="text-wrapper">
+            <h3>INFO</h3>
+            <h3>EXPLORE</h3>
+            <h3>FAQ</h3>
+        </div>
+
+        <div className="logo_wrapper">
+            <img src={pic} className='logo' alt='' />
+        </div>
+
+        <div className="icons">
+        <IoPersonOutline />
+        <TiShoppingCart />
+
+        </div>
+        
+    </div>
+  )
+}
+
+export default Navbar
