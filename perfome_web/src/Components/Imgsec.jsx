3d4bd622bf1b1d24c7ead532d2a8f5b22db46f50
Ramadan <daudalaramadan2@gmail.com>
2024-05-06 00:45:16 +0000

    hero sec

--- perfome_web/src/Components/Imgsec.jsx
+++ perfome_web/src/Components/Imgsec.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import pic1 from '../Assets/240_F_277304950_m2UpW5QJmsTNDoEylhBmG3rljuBtYUGA.jpg'
+import pic2 from '../Assets/240_F_679658169_1WS6B1vKp02TUW6dm0JDHaYVYamOxnEs.jpg'
+import pic3 from '../Assets/240_F_68533066_PYa50hMrSVJQvU5nkjVOQAK1judrFf5U.jpg'
+import pic4 from '../Assets/Seasons of Scent - A Lovely Inconsequence.jpeg'
+import './Imgsec.css'
 
 const imgsec = () => {
   return (
-    <div>imgsec</div>
+    <div className='imgsec-wrapper'>
+        <div className="img1">
+            <img src={pic1} alt="" srcset="" />
+            <h4>Seasonal Scents</h4>
+        </div>
+        <div className="img2">
+            <img src={pic2} alt="" srcset="" />
+            <h4>Travel With Creed</h4>
+        </div>
+        <div className="img3">
+            <img src={pic3} alt="" srcset="" />
+            <h4>24/7</h4>
+        </div>
+        <div className="img4">
+            <img src={pic4} alt="" srcset="" />
+            <h4>One Spray <br /> Wonder</h4>
+        </div>
+    </div>
   )
 }
 
